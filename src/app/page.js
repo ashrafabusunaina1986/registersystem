a56d1cff8b0fa2520d97e09e999bc5f60ae26794
ashrafabusunaina1986 <gffcnh>
2024-06-22 21:30:56 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -77,7 +77,7 @@ export default function Home() {
           </a>
         </div>
       )}
-      <div className="sm:block hidden mb-10">
+      <div className="sm:block hidden sm:mb-10">
         <Box />
       </div>
 
