5ada7aa5a1ba1f2597d43037049bc0896182b7c3
ashrafabusunaina1986 <101833519+ashrafabusunaina1986@users.noreply.github.com>
2024-08-19 21:42:15 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,18 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-function Header({token}) {
-  // const [token, setToken] = useState("");
-  // const logout = async () => {
-  //   const res = await fetch("api/logout", {
-  //     method: "POST",
-  //   });
-  //   if (!res) return;
-  //   const data = await res.json();
-  //   console.log(data);
-  //   setToken(data.token);
-  // };
+function Header({ token }) {
   return (
-    <header className="flex flex-row-reverse items-center justify-between w-full px-8 py-5 bg-gray-200">
+    <header className="flex sm:flex-col-reverse gap-4 flex-row-reverse items-center justify-between w-full px-8 py-5 bg-gray-600 shadow-md shadow-gray-400">
       <ul className="flex items-center gap-5 w-max list-none">
         {!token ? (
           <>
             <li>
               <Link
                 href={"/signup"}
-                className="inline-flex items-center justify-center  px-5 py-3  font-semibold text-white bg-black rounded-md hover:bg-gray-700 hover:text-gray-50"
+                className="inline-flex items-center justify-center  px-5 py-3 sm:px-3 sm:py-1 sm:text-xs font-semibold text-slate-300  rounded-3xl hover:bg-gray-300 hover:text-gray-950 bg-zinc-400"
               >
                 Sign up
               </Link>
@@ -30,8 +20,7 @@ function Header({token}) {
             <li>
               <Link
                 href={"/login"}
-                className="inline-flex items-center justify-center px-5 py-3 font-semibold text-white bg-black rounded-md hover:bg-gray-700 hover:text-gray-50"
-              >
+                className="inline-flex items-center justify-center  px-5 py-3 sm:px-3 sm:py-1 sm:text-xs font-semibold text-slate-300  rounded-3xl hover:bg-gray-300 hover:text-gray-950 bg-zinc-400"              >
                 Log in
               </Link>
             </li>
@@ -39,9 +28,9 @@ function Header({token}) {
         ) : (
           <>
             <li>
-              <Link 
+              <Link
                 href={"/logout"}
-                className="inline-flex items-center justify-center px-5 py-3 font-semibold text-white bg-black rounded-md hover:bg-gray-700 hover:text-gray-50"
+                className="inline-flex items-center justify-center px-5 py-3 font-semibold text-slate-400 rounded-sm hover:bg-gray-500 hover:text-gray-50"
               >
                 Log out
               </Link>
